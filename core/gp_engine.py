78889ab9e31d96ea0cfc81483a37b0a01418ddf5
HelenaMihaljcic <mihaljcichelena@gmail.com>
2026-09-26 17:13:53 +0200

    valjda je sad dobro

--- core/gp_engine.py
+++ core/gp_engine.py
@@ -1,56 +1,36 @@
 import operator
 import math
 import random
+import time
+from typing import Any, Tuple
+from PyQt6.QtCore import QThread, pyqtSignal
 
 from deap import base, creator, tools, gp
-
 from grid import Grid
-from astar import run_a_star, heuristic_dijkstra
+from a_star import run_a_star, heuristic_dijkstra
+
+#from .grid import Grid
+#from .a_star import run_a_star, heuristic_dijkstra
 
 
- 
-# PROTECTED DIVISION
- 
-
-def protected_div(left, right):
-    if abs(right) < 1e-6:
+def protected_div(left: float, right: float) -> float:
+    if abs(right) < 0.001:
         return 1.0
-
     return left / right
 
 
- 
-# FITNESS AND INDIVIDUAL
- 
 
 if not hasattr(creator, "FitnessMax"):
-    creator.create(
-        "FitnessMax",
-        base.Fitness,
-        weights=(1.0,)
-    )
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMax)
 
-if not hasattr(creator, "Individual"):
-    creator.create(
-        "Individual",
-        gp.PrimitiveTree,
-        fitness=creator.FitnessMax
-    )
-
-
- 
-# PRIMITIVE SET
- 
-
-pset = gp.PrimitiveSet("MAIN", 5)
-
-pset.addPrimitive(operator.add, 2)
-pset.addPrimitive(operator.sub, 2)
-pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(protected_div, 2)
-
-pset.addPrimitive(min, 2)
-pset.addPrimitive(max, 2)
+pset = gp.PrimitiveSetTyped("MAIN", [float] * 5, float)
+pset.addPrimitive(operator.add, [float, float], float)
+pset.addPrimitive(operator.sub, [float, float], float)
+pset.addPrimitive(operator.mul, [float, float], float)
+pset.addPrimitive(protected_div, [float, float], float)
+pset.addPrimitive(min, [float, float], float)
+pset.addPrimitive(max, [float, float], float)
 
 pset.renameArguments(ARG0="dx")
 pset.renameArguments(ARG1="dy")
@@ -58,349 +38,131 @@ pset.renameArguments(ARG2="d_manhattan")
 pset.renameArguments(ARG3="d_euclidean")
 pset.renameArguments(ARG4="obs_density")
 
-
- 
-# TOOLBOX
- 
-
 toolbox = base.Toolbox()
-
-toolbox.register(
-    "expr",
-    gp.genHalfAndHalf,
-    pset=pset,
-    min_=1,
-    max_=2
-)
-
-toolbox.register(
-    "individual",
-    tools.initIterate,
-    creator.Individual,
-    toolbox.expr
-)
-
-toolbox.register(
-    "population",
-    tools.initRepeat,
-    list,
-    toolbox.individual
-)
-
-toolbox.register(
-    "compile",
-    gp.compile,
-    pset=pset
-)
-
-
- 
-# GRID
- 
-
-grid = Grid()
-
-
- 
-# START AND GOAL
- 
-
-start = grid.start
-goal = grid.goal
-
-
- 
-# OPTIMAL PATH
- 
-
-optimal_path = run_a_star(
-    grid,
-    start,
-    goal,
-    heuristic_dijkstra
-)
-
-optimal_length = len(optimal_path)
-
-
- 
-# HEURISTIC
- 
-
-def calculate_heuristic(
-    heuristic,
-    current,
-    goal,
-    grid
-):
-    dx = goal[0] - current[0]
-    dy = goal[1] - current[1]
-
-    d_manhattan = (
-        abs(dx) +
-        abs(dy)
-    )
-
-    d_euclidean = math.sqrt(
-        dx * dx +
-        dy * dy
-    )
-
-    obs_density = 0.0
-
-    return heuristic(
-        dx,
-        dy,
-        d_manhattan,
-        d_euclidean,
-        obs_density
-    )
-
-
- 
-# EVALUATION
- 
-
-def evaluate(individual):
-
-    heuristic = toolbox.compile(
-        expr=individual
-    )
-
-    def gp_heuristic(current, goal, grid):
-
-        return calculate_heuristic(
-            heuristic,
-            current,
-            goal,
-            grid
-        )
-
-    path, visited = run_a_star(
-        grid,
-        start,
-        goal,
-        gp_heuristic
-    )
-
-     
-    # NO PATH
-     
-
-    if not path:
-        return (-1000.0,)
-
-     
-    # PATH LENGTH
-     
-
-    path_length = len(path)
-
-    length_difference = abs(
-        path_length - optimal_length
-    )
-
-     
-    # VISITED NODES
-     
-
-    visited_count = len(visited)
-
-     
-    # TREE COMPLEXITY
-     
-
-    tree_depth = individual.height
-
-     
-    # FITNESS
-     
-
-    fitness = (
-        -length_difference
-        -0.01 * visited_count
-        -0.01 * tree_depth
-    )
-
-    return (fitness,)
-
-
-toolbox.register(
-    "evaluate",
-    evaluate
-)
-
-
- 
-# SELECTION
- 
-
-toolbox.register(
-    "select",
-    tools.selTournament,
-    tournsize=3
-)
-
-
- 
-# CROSSOVER
- 
-
-toolbox.register(
-    "mate",
-    gp.cxOnePoint
-)
-
-
- 
-# MUTATION
- 
-
-toolbox.register(
-    "mutate",
-    gp.mutUniform,
-    expr=toolbox.expr,
-    pset=pset
-)
-
-
- 
-# POPULATION
- 
-
-population = toolbox.population(
-    n=20
-)
-
-
- 
-# INITIAL EVALUATION
- 
-
-for individual in population:
-
-    individual.fitness.values = (
-        toolbox.evaluate(individual)
-    )
-
-
- 
-# EVOLUTION
- 
-
-number_of_generations = 20
-
-for generation in range(
-    number_of_generations
-):
-
-     
-    # SELECTION
-     
-
-    selected = toolbox.select(
-        population,
-        len(population)
-    )
-
-     
-    # CLONE
-     
-
-    offspring = list(
-        map(
-            toolbox.clone,
-            selected
-        )
-    )
-
-     
-    # CROSSOVER
-     
-
-    for child1, child2 in zip(
-        offspring[::2],
-        offspring[1::2]
-    ):
-
-        if len(child1) > 1 and len(child2) > 1:
-
-            toolbox.mate(
-                child1,
-                child2
-            )
-
-            del child1.fitness.values
-            del child2.fitness.values
-
-     
-    # MUTATION
-     
-
-    for mutant in offspring:
-
-        toolbox.mutate(
-            mutant
-        )
-
-        del mutant.fitness.values
-
-     
-    # EVALUATION
-     
-
-    for individual in offspring:
-
-        if not individual.fitness.valid:
-
-            individual.fitness.values = (
-                toolbox.evaluate(individual)
-            )
-
-     
-    # NEW GENERATION
-     
-
-    population = offspring
-
-     
-    # BEST
-     
-
-    best_individual = tools.selBest(
-        population,
-        1
-    )[0]
-
-    print(
-        "Generation:",
-        generation,
-        "| Best:",
-        best_individual,
-        "| Fitness:",
-        best_individual.fitness.values[0]
-    )
-
-
- 
-# FINAL RESULT
- 
-
-best_individual = tools.selBest(
-    population,
-    1
-)[0]
-
-print()
-print("Best individual:")
-print(best_individual)
-
-print(
-    "Fitness:",
-    best_individual.fitness.values[0]
-)
-
-print(
-    "Tree depth:",
-    best_individual.height
-)
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=3)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("compile", gp.compile, pset=pset)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=7))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=7))
+
+
+class GPEngineThread(QThread):
+    generation_done = pyqtSignal(int, float, float, object, str)
+    evolution_finished = pyqtSignal()
+
+    def __init__(self, grid: Grid, pop_size: int, ngen: int, cxpb: float, mutpb: float) -> None:
+        super().__init__()
+        self.grid = grid.copy()
+        self.pop_size = pop_size
+        self.ngen = ngen
+        self.cxpb = cxpb
+        self.mutpb = mutpb
+
+        self._is_paused = False
+        self._is_stopped = False
+
+        
+        opt_path, _, _ = run_a_star(self.grid, self.grid.start, self.grid.goal, heuristic_dijkstra)
+        self.optimal_length = len(opt_path)
+
+    def pause(self) -> None:
+        self._is_paused = True
+
+    def resume(self) -> None:
+        self._is_paused = False
+
+    def stop(self) -> None:
+        self._is_stopped = True
+
+    def evaluate(self, individual: Any) -> Tuple[float]:
+        try:
+            func = toolbox.compile(expr=individual)
+        except Exception:
+            return (-100000.0,)
+
+        def h_wrapper(x: int, y: int, gx: int, gy: int, grid_obj: Grid) -> float:
+            dx = float(abs(x - gx))
+            dy = float(abs(y - gy))
+            dm = dx + dy
+            de = math.sqrt(dx ** 2 + dy ** 2)
+            od = grid_obj.get_obstacle_density(x, y)
+
+            try:
+                val = func(dx, dy, dm, de, od)
+                if math.isnan(val) or math.isinf(val):
+                    return 10000.0
+                return max(0.0, val)
+            except Exception:
+                return 10000.0
+
+        path, visited, _ = run_a_star(self.grid, self.grid.start, self.grid.goal, h_wrapper)
+
+        if not path:
+            no_path_penalty = 1000.0
+            path_len = 0.0
+        else:
+            no_path_penalty = 0.0
+            path_len = float(len(path))
+
+        visited_nodes = float(len(visited))
+        tree_depth = float(individual.height)
+
+        fitness = -((3.0 * visited_nodes) + (10.0 * abs(path_len - self.optimal_length)) + (
+                    0.1 * tree_depth) + no_path_penalty)
+        return (fitness,)
+
+    def run(self) -> None:
+        pop = toolbox.population(n=self.pop_size)
+        hof = tools.HallOfFame(1)
+
+        # Evaluate initial population
+        for ind in pop:
+            ind.fitness.values = self.evaluate(ind)
+
+        hof.update(pop)
+        self.emit_stats(0, pop, hof)
+
+        for gen in range(1, self.ngen + 1):
+            while self._is_paused and not self._is_stopped:
+                time.sleep(0.1)
+
+            if self._is_stopped:
+                break
+
+            offspring = toolbox.select(pop, len(pop))
+            offspring = list(map(toolbox.clone, offspring))
+
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if random.random() < self.cxpb:
+                    toolbox.mate(child1, child2)
+                    del child1.fitness.values
+                    del child2.fitness.values
+
+            for mutant in offspring:
+                if random.random() < self.mutpb:
+                    toolbox.mutate(mutant)
+                    del mutant.fitness.values
+
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            for ind in invalid_ind:
+                ind.fitness.values = self.evaluate(ind)
+
+            pop[:] = offspring
+            hof.update(pop)
+
+            self.emit_stats(gen, pop, hof)
+
+        self.evolution_finished.emit()
+
+    def emit_stats(self, gen: int, pop: list, hof: Any) -> None:
+        fits = [ind.fitness.values[0] for ind in pop]
+        avg_fit = sum(fits) / len(fits)
+        best_fit = hof[0].fitness.values[0]
+        best_expr = str(hof[0])
+        self.generation_done.emit(gen, best_fit, avg_fit, hof[0], best_expr)
